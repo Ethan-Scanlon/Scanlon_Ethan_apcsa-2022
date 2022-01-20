69486600713577198f667472b2c8cc2fa05baed8
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-01-19 23:13:19 -0800

    Trap done

---
+++ Unit-02/src/Trap.java
@@ -0,0 +1,15 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Ethan Scanlon
+//Date - 1/19/22
+//Class - CSA per 1
+//Lab  -  Trap
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		
+		return (double)0.5*(base1 + base2) * height;
+	}
+}
+
