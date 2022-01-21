c985da569fa213d9ada6eaf25ad5339cc9104ceb
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-01-20 17:32:44 -0800

    Unit 3 done

--- Unit-03/src/Distance.java
+++ Unit-03/src/Distance.java
@@ -17,18 +17,31 @@ public class Distance
 
 	public Distance()
 	{
+		setCoordinates(0,0,0,0);
+		distance = 0.0;
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+		setCoordinates(x1, y1, x2, y2);
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void calcDistance()
 	{
+		distance = sqrt(pow((xTwo-xOne), 2) + pow((yTwo-yOne),2));
+//		distance = 1000*distance;
+//		distance = Math.ceil(distance);
+//		distance = distance /1000;
+		String str = Double.toString(distance);
+				
 	}
 	
 	public double getDistance()
@@ -38,6 +51,7 @@ public class Distance
 	
 	public void print()
 	{
+		System.out.printf("%.3f\n", distance);
 	}
 	
 	//complete print or the toString
