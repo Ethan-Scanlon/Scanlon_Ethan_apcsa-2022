2cd7b4bca0f10657776976ab4dcf7d8733968099
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-01-27 23:24:50 -0800

    Unit 5 complete

--- Unit-05/src/StringEquality.java
+++ Unit-05/src/StringEquality.java
@@ -14,23 +14,36 @@ public class StringEquality
 
 	public StringEquality()
 	{
+		wordOne = "";
+		wordTwo = "";
 	}
 
 	public StringEquality(String one, String two)
 	{
+		setWords(one, two);
 	}
 
 	public void setWords(String one, String two)
 	{
+		wordOne = one;
+		wordTwo = two;
 	}
 
 	public boolean checkEquality( )
 	{
-		return false;
+		if(wordOne.equals(wordTwo) == true) //.equals() and compareTo() can work here
+			return true;
+		
+		else
+			return false;
 	}
 
 	public String toString()
 	{
+		if(checkEquality() == true)
+			return wordOne + " has the same letters as " + wordTwo + "\n";
+		
+	else
 		return wordOne + " does not have the same letters as " + wordTwo + "\n";
 	}
 }
--- Unit-05/src/WordsCompareRunner.java
+++ Unit-05/src/WordsCompareRunner.java
@@ -16,35 +16,35 @@ public class WordsCompareRunner
 	   
 	   
 	   WordsCompare test = new WordsCompare("abe", "ape");
-	   test.compare();
+	  
 	   System.out.println(test);
 
 	   WordsCompare test2 = new WordsCompare("giraffe", "gorilla");
-	   test2.compare();
+	  
 	   System.out.println(test2);
 	   
 	   WordsCompare test3 = new WordsCompare("one", "two");
-	   test3.compare();
+	  
 	   System.out.println(test3);
 	   
 	   WordsCompare test4 = new WordsCompare("fun", "funny");
-	   test4.compare();
+	  
 	   System.out.println(test4);
 	   
 	   WordsCompare test5 = new WordsCompare("123", "19");
-	   test5.compare();
+	  
 	   System.out.println(test5);
 	   
 	   WordsCompare test6 = new WordsCompare("193", "1910");
-	   test6.compare();
+	   
 	   System.out.println(test6);
 	   
 	   WordsCompare test7 = new WordsCompare("goofy", "godfather");
-	   test7.compare();
+	  
 	   System.out.println(test7);
 	   
 	   WordsCompare test8 = new WordsCompare("funnel", "fun");
-	   test8.compare();
+	  
 	   System.out.println(test8);
 	}
 }
