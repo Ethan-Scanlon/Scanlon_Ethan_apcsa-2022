faf029705b7159bbf4dc49ff5c82641512c1b17f
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-01 20:30:22 -0800

    Word complete

--- Unit-06/src/WordRunner.java
+++ Unit-06/src/WordRunner.java
@@ -8,6 +8,21 @@ public class WordRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		//add test cases	
+		Word test = new Word("Hello");
+		System.out.println(test);
+		
+		Word test2 = new Word("World");
+		System.out.println(test2);
+		
+		Word test3 = new Word("Jukebox");
+		System.out.println(test3);
+		
+		Word test4 = new Word("TCEA");
+		System.out.println(test4);
+		
+		Word test5 = new Word("UIL");
+		System.out.println(test5);
+		
 	}
 }
