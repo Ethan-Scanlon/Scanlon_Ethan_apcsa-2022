d61ffd897c427c65ef9206502c1c3996334fbd2c
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-01 20:03:17 -0800

    TriangleOne complete

--- Unit-06/src/TriangleOneRunner.java
+++ Unit-06/src/TriangleOneRunner.java
@@ -8,6 +8,21 @@ public class TriangleOneRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		//add test cases
+		TriangleOne test = new TriangleOne("hippo");
+		test.print();
+		
+		TriangleOne test2 = new TriangleOne("abcd");
+		test2.print();
+		
+		TriangleOne test3 = new TriangleOne("it");
+		test3.print();
+		
+		TriangleOne test4 = new TriangleOne("a");
+		test4.print();
+		
+		TriangleOne test5 = new TriangleOne("chicken");
+		test5.print();
+		
 	}
 }
