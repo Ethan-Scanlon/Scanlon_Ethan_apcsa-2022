309184dd43afac38c00973e7815d201950ef010d
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-01 19:45:04 -0800

    countpairs complete

---
+++ Unit-06/src/LetterRemover.java
@@ -0,0 +1,41 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class LetterRemover
+{
+   private String sentence;
+   private char lookFor;
+
+	public LetterRemover()
+	{
+		sentence = "";
+		lookFor = 0;
+		//call set
+	}
+
+	//add in second constructor
+	public LetterRemover(String s, char rem)
+	{
+		setRemover(s, rem);
+	}
+	
+	public void setRemover(String s, char rem)
+	{
+		sentence = s;
+		lookFor = rem;
+	}
+
+	public String removeLetters()
+	{
+		String cleaned=sentence;
+		return cleaned;
+	}
+
+	public String toString()
+	{
+		return sentence + " - letter to remove " + lookFor;
+	}
+}
---
+++ Unit-06/src/LetterRemoverRunner.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class LetterRemoverRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(in);
+		
+		
+		//add test cases		
+											
+	}
+}
