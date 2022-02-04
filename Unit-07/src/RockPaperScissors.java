970adae51725753087e80653752c02dc9705fd75
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-04 09:28:52 -0800

    RPS progress

--- Unit-07/src/RockPaperScissors.java
+++ Unit-07/src/RockPaperScissors.java
@@ -4,6 +4,7 @@
 
 import java.util.Scanner;
 import static java.lang.System.*;
+import java.lang.Math;
 
 public class RockPaperScissors
 {
@@ -12,25 +13,42 @@ public class RockPaperScissors
 
 	public RockPaperScissors()
 	{
+		playChoice = "";
+		compChoice = "";
 	}
 
 	public RockPaperScissors(String player)
 	{
+		setPlayers(player);
 	}
 
 	public void setPlayers(String player)
 	{
+		playChoice = player;
+		int temp = (int) Math.ceil(3*Math.random());
+		 if(temp==0) compChoice = "R";
+		 if(temp==1) compChoice = "P";
+		 if (temp==2) compChoice = "S";
 	}
 
 	public String determineWinner()
 	{
 		String winner="";
+		if(playChoice == "R" && compChoice == "P") winner = "computer";
+		if(playChoice == "P" && compChoice == "S") winner = "computer";
+		if(playChoice == "S" && compChoice =="R") winner = "computer";
+		if((playChoice =="R" && compChoice =="R") || (playChoice=="P"&&compChoice=="P") || (playChoice=="S"&&compChoice=="S"))
+				winner="draw";
+		if(playChoice=="R"&&compChoice=="S") winner="player";
+		if(playChoice=="P"&& compChoice == "R") winner="player";
+		if(playChoice=="S"&&compChoice=="P")winner="player";
 		return winner;
 	}
 
 	public String toString()
 	{
-		String output="";
+		String output="player had " + playChoice + "\n" + "computer had "+ compChoice
+				+ "\n" + determineWinner() + "wins!";
 		return output;
 	}
 }
