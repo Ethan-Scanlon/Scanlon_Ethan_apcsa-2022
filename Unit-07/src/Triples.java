7bceedbbecdbb7abc45d03e1a2c293941e97e4af
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-06 17:53:36 -0800

    Triples complete

--- Unit-07/src/Triples.java
+++ Unit-07/src/Triples.java
@@ -15,28 +15,57 @@ public class Triples
 
 	public Triples(int num)
 	{
-
+		setNum(num);
 
 	}
 
 	public void setNum(int num)
 	{
-
+		number = num;
 
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
-		int max = 0;
+//		int max = 0;
+		for(int i=a/2+1; i>0; i--) 
+			if(a%i == 0)
+				for(int j=b/2+1; j>0; j--)
+					if(b%j == 0)
+						for(int k=c/2+1; k>0; k--)
+							if(c%k == 0)
+//								if(i>j && i>k)
+//									max=i;
+//								else if(k>j && k>i)
+//									max=k;
+//								else if(j>k && j>i)
+//									max=j;
+//								else if(max==1 && max==i && max==j && max ==k)
+//									return 1;
+								if(i==1 && i==j && i==k)
+									return 1;
+									
+									
+									
+					
+		
 
-
-
-		return 1;
+		return 0;
+		
 	}
 
 	public String toString()
 	{
 		String output="";
+		for(int a = 1; a<=number; a++ ) 
+			for(int b = 1; b<=number; b++)
+				for(int c=1; c<=number; c++)
+					if(a*a + b*b == c*c)
+						if((a%2==1 && b%2==0)||(a%2==0 && b%2==1))
+							if(c%2==1)
+								if(greatestCommonFactor(a, b, c) == 1)
+									if(a<b)
+										output += a + " " + b + " " + c + "\n";
 
 
 
--- Unit-07/src/TriplesRunner.java
+++ Unit-07/src/TriplesRunner.java
@@ -4,9 +4,19 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class TriplesRunner
 {
    public static void main(String args[])
    {
+	   
+	int number = 0;
+	Scanner keyboard = new Scanner(in);
+	number = keyboard.nextInt();
+	Triples test = new Triples(number);
+	System.out.println(test);
+	   
+	   
 	}
 }
