6ac01180e726d7704c06abbad6447627204c536e
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-08 09:29:55 -0800

    rayoddtoevenprog

--- Unit-08/src/RayOddToEven.java
+++ Unit-08/src/RayOddToEven.java
@@ -5,8 +5,24 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int oddIndex = -1;
+		int evenIndex = -1;
+	
+		for(int i = 0; i<ray.length; i++) {
+			if(ray[i] % 2 == 1) oddIndex = i;
+		
+			for(int j = oddIndex; j<ray.length; j++) {
+				if(ray[j] % 2 ==0) evenIndex = j;
+				
+				if(oddIndex==i && evenIndex==j) {
+					return evenIndex-oddIndex;
+				}
+				
+			}
+			
+		}
+		return -1;
 	}
 }
