d42c842707bde577a37dbc874bfc0b12dbc4b472
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-09 17:48:01 -0800

    Unit 8 complete

--- Unit-08/src/RayDown.java
+++ Unit-08/src/RayDown.java
@@ -9,6 +9,18 @@ public class RayDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
+		boolean test = true;
+		for(int i = 0; i<numArray.length-1; i++) {
+			if(numArray[i]>numArray[i+1]) {
+				test = true;
+			}
+			if(numArray[i]<=numArray[i+1]) {
+				test = false;
+			}
+			if (test==false) {
+				return false;
+			}
+		}
 		return true;
 	}	
 }
