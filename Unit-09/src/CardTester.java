551732e52b1aafd0b4cbd194b5734d72c380af81
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-02-15 21:37:12 -0800

    activity 1 complete

--- Unit-09/src/CardTester.java
+++ Unit-09/src/CardTester.java
@@ -9,5 +9,21 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card sixClubs = new Card("6", "clubs", 6);
+		Card queenHearts = new Card("queen", "hearts", 12);
+		Card tenSpades = new Card("10", "spades", 10);
+		
+		System.out.println(sixClubs.pointValue());
+		System.out.println(sixClubs.rank());
+		System.out.println(sixClubs.suit());
+		System.out.println(sixClubs);
+		System.out.println(queenHearts.pointValue());
+		System.out.println(queenHearts.rank());
+		System.out.println(queenHearts.suit());
+		System.out.println(queenHearts);
+		System.out.println(tenSpades.pointValue());
+		System.out.println(tenSpades.rank());
+		System.out.println(tenSpades.suit());
+		System.out.println(tenSpades);
 	}
 }
