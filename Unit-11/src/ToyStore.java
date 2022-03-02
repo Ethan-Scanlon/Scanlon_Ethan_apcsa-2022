aa235747b2b39262f485eb4d07f92847de4f2022
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-03-02 09:15:33 -0800

    toystore progress

--- Unit-11/src/ToyStore.java
+++ Unit-11/src/ToyStore.java
@@ -17,14 +17,36 @@ public class ToyStore
 		toyList = new ArrayList<Toy>();
 	}
 
-	public void loadToys( String toys )
-	{
+	public void loadToys( String toys ) {
+		int count = 0;
 		String[] check = toys.split(" ", 0);
-		for(int i = 0; i<toys.length(); i++)
+		for(int i = 0; i<toys.length(); i++) {
+			count = 0;
+			Toy newToy = new Toy(check[i]);
+			for(int j = i; j<check.length; j++) {
+				if(check[i].compareTo(check[i]) == 0) {
+					count++;
+				}
+			}
+		newToy.setCount(count);
+		toyList.add(i, newToy);
 	}
-  
+		for(int i=1; i<toyList.size(); i++) {
+			for(int j=i-1; j>-1; j--) {
+				if(toyList.get(i).getName().compareTo(toyList.get(j).getName()) == 0) {
+					toyList.remove(i);
+					i--;
+				}
+			}
+		}
+	}
   	public Toy getThatToy( String nm )
   	{
+  		for(Toy toy : toyList) {
+  			if(toy.getName().compareTo(nm) == 0) {
+  				return toy;
+  			}
+  		}
   		return null;
   	}
   
@@ -39,6 +61,6 @@ public class ToyStore
   	  
 	public String toString()
 	{
-	   return "";
+	   return toyList.toString();
 	}
 }
