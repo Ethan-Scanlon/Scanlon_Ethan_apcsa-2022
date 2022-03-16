1fb9d6d2df9e4fee77e558aca58ed4c7198aaf72
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-03-15 19:50:33 -0700

    recursion done

--- Unit-14/src/RecursionFunThree.java
+++ Unit-14/src/RecursionFunThree.java
@@ -20,6 +20,12 @@ public class RecursionFunThree
 	 */
 	public static int luckyThrees( long number )
 	{
-		return 0;
+		if(number == 0) return 0;
+		
+		if(number/10 != 0 && number % 10 == 3) {
+		return 1+ luckyThrees(number/10);
+		}
+		return luckyThrees(number/10);
+		
 	}
 }
