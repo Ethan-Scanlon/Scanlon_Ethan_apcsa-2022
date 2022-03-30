1972514be5adf14a8950567647cfe31c7b178235
Ethan-Scanlon <ethanh.tran05@gmail.com>
2022-03-30 10:36:08 -0700

    unit 13

--- Unit-13/src/NumberShifter.java
+++ Unit-13/src/NumberShifter.java
@@ -12,9 +12,28 @@ public class NumberShifter
 {
 	public static int[] makeLucky7Array( int size)
 	{
-		return null;
+		int[] monkey = new int[size];
+		for(int i = 0; i< size; i++) {
+			monkey[i] = (int) (10*(Math.random())) +1;
+		}
+		return monkey;
 	}
-	public static void shiftEm(int[] array)
+	public static void shiftEm(int[] stuff)
 	{
+		System.out.println(Arrays.toString(stuff));
+		for(int i=0; i< stuff.length-1; i++){
+		      int min = i;
+		      for(int j = i+1; j<  stuff.length; j++)
+		      {
+		         if( stuff[j] == 7 )
+		            min = j;    		
+		      }
+		      if(min != i) {
+		         int temp = stuff[min];
+		         stuff[min] = stuff[i];
+		         stuff[i] = temp;   	
+		      }
+		   }
+		System.out.println(Arrays.toString(stuff));
 	}
 }
